9e880a2814b2674882ea560ea87c82c3dadcb762
tigrelau <lauratigreamaral@gmail.com>
2025-10-16 20:08:54 -0300

    add/save1

---
+++ src/pages/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from '/..Navbar.jsx'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
+      <main className="p-6">
+      </main>
+    </div>
+  );
+}
